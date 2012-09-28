d7189472cb222f33ed448d1eba65573c63730e85
aherrero <alexhc89@gmail.com>
2012-09-28 17:29:59 +0200

    try localizer, without exit

---
+++ tests/CMakeFiles/localizerTest.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/mrdevgit/tests/glutapp.cpp" "/home/alex/mrdevgit/tests/CMakeFiles/localizerTest.dir/glutapp.cpp.o"
+  "/home/alex/mrdevgit/tests/testlocalizer.cpp" "/home/alex/mrdevgit/tests/CMakeFiles/localizerTest.dir/testlocalizer.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
